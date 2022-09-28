75281455ebb515a9609317f068cc1a10e784be7f
Roxane <a12030167@unet.univie.ac.at>
2022-09-28 14:44:05 +0200

    First draft of GNNs (still outputing wrong predictions)

--- models/mlp.py
+++ models/mlp.py
@@ -7,11 +7,13 @@ class MLP(torch.nn.Module):
         super().__init__()
         torch.manual_seed(42)
         self.lin1 = Linear(in_dim, h_dim)
-        self.lin2 = Linear(h_dim, out_dim)
+        self.lin2 = Linear(h_dim, h_dim)
+        self.lin3 = Linear(h_dim, out_dim)
 
-    def forward(self, x):
+    def forward(self, x, edge_index):
         x = self.lin1(x)
-        x = x.relu()
-        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(x)
         x = self.lin2(x)
+        x = F.relu(x)
+        x = self.lin3(x)
         return x
