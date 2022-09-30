77aacfb27d1a5c88eaada636ba6af5c68b4a10e8
Roxane <a12030167@unet.univie.ac.at>
2022-09-30 14:49:20 +0200

    Minor changes

--- models/graph_neural_nets.py
+++ models/graph_neural_nets.py
@@ -1,32 +1,37 @@
 import torch
 import torch.nn.functional as F
 from torch.nn import Sequential, Linear, BatchNorm1d, ReLU
-from torch_geometric.nn import GCNConv, GINConv
+from torch_geometric.nn import GINConv
 
-class GCN(torch.nn.Module):
-    def __init__(self, in_dim, h_dim, out_dim):
-        super().__init__()
-        self.conv1 = GCNConv(in_dim, h_dim)
-        self.conv2 = GCNConv(h_dim, h_dim)
-        self.conv3 = GCNConv(h_dim, h_dim)
-        self.lin1 = Linear(h_dim*3, h_dim*3)
-        self.lin2 = Linear(h_dim*3, out_dim)
+def train(model, loader, class_weights):
+        model.train()
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-3)
+        criterion = torch.nn.CrossEntropyLoss(weight=torch.Tensor(class_weights))
+        loss = 0
+        total_num_instances = 0
+        for data in loader:
+            optimizer.zero_grad()  # Clear gradients
+            _, out = model(data.x, data.edge_index)  # Perform a forward pass
+            batch_loss = criterion(out, data.y.T.long())  # Compute loss function
+            loss += batch_loss * len(data.batch)
+            total_num_instances += len(data.batch)
+            batch_loss.backward()  # Derive gradients
+            optimizer.step()  # Update parameters based on gradients
+        loss /= total_num_instances
+        return loss
 
-    def forward(self, x, edge_index):
-        # Node embeddings
-        h1 = self.conv1(x, edge_index)
-        h2 = self.conv2(h1, edge_index)
-        h3 = self.conv3(h2, edge_index)
+@torch.no_grad()
+def test(model, loader):
+    model.eval()
+    predictions = []
+    true_labels = []
+    for data in loader:
+        _, out = model(data.x, data.edge_index)  # Perform a forward pass
+        predictions.append(out.argmax(dim=1))   # Store the class with the highest probability for each data point
+        true_labels.append(data.y)
+    pred, true = torch.cat(predictions, dim=0).numpy(), torch.cat(true_labels, dim=0).numpy()
+    return pred, true
 
-        # Concatenate graph embeddings
-        h = torch.cat((h1, h2, h3), dim=1)
-
-        # Classify
-        h = self.lin1(h)
-        h = h.relu()
-        h = self.lin2(h)
-
-        return h, F.softmax(h, dim=1)
 
 class GIN(torch.nn.Module):
     def __init__(self, in_dim, h_dim, out_dim):
@@ -64,3 +69,4 @@ class GIN(torch.nn.Module):
         h = self.lin2(h)
 
         return h, F.softmax(h, dim=1)
+        
