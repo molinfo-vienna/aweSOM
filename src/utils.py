1dea852b8bd49f45fe8094aa83e23307891c402d
Roxane <a12030167@unet.univie.ac.at>
2022-10-19 15:59:24 +0200

    added roc_auc_display

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import logging
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc, roc_curve
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
@@ -34,3 +36,23 @@ class EarlyStopping:
         else:
             logging.warning("Unsupported optimizing criterion.")
 
+
+def roc_curve_display(y_true, y_pred, output_path):
+    fpr, tpr, _ = roc_curve(y_true, y_pred)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    plt.plot(
+        fpr,
+        tpr,
+        color="darkorange",
+        lw=2,
+        label="ROC curve (area = %0.2f)" % roc_auc,
+    )
+    plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve, Validation Set")
+    plt.legend(loc="lower right")
+    plt.savefig(output_path)
