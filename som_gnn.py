1e9c142e735213f423bfa297e65e98a73bb5b51c
Roxane <a12030167@unet.univie.ac.at>
2022-11-14 17:16:35 +0100

    added top2-accuracy and tested model on xenosite data

--- som_gnn.py
+++ som_gnn.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import matthews_corrcoef, accuracy_score, \
+from sklearn.metrics import matthews_corrcoef, \
     jaccard_score, precision_score, recall_score, roc_auc_score, \
         ConfusionMatrixDisplay, PrecisionRecallDisplay
 from sklearn.utils.class_weight import compute_class_weight
@@ -13,7 +13,7 @@ from tqdm import tqdm
 
 from src.process_input_data import process_data
 from src.pyg_dataset_creator import SOM
-from src.graph_neural_nets import GIN, GAT, train, test
+from src.graph_neural_nets import GIN, GAT, train_oversampling, train, test
 from src.utils import plot_losses, plot_roc_curve
 
 
@@ -24,12 +24,15 @@ def main():
     torch.manual_seed(42)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    data_path = 'data_xenosite/'
+    data_name = 'xenosite_preprocessed.sdf'
+    output_path = 'output_xenosite/'
 
     # Process SDF input data to create PyTorch Geometric custom dataset
-    #process_data(data_path='data/dataset_preprocessed.sdf', output_path='output/')
+    #process_data(data_path=data_path, data_name=data_name, output_path=output_path)
 
     # Create/Load Custom PyTorch Geometric Dataset
-    dataset = SOM(root='data')
+    dataset = SOM(root=data_path)
 
     # Print dataset info
     print(f'Number of graphs: {len(dataset)}')
@@ -75,17 +78,17 @@ def main():
 
     """ ---------- Train Model ---------- """
 
-    dir = 'output/dicebce/'
+    dir = output_path+'dicebce/'
     train_losses = []
     val_losses = []
     print('Training...')
     for _ in tqdm(range(epochs)):
         train_loss = train(model, train_loader, lr, weight_decay, device)
         train_losses.append(train_loss.item())
-        val_loss, val_pred, val_true = test(model, val_loader, device)
+        val_loss, _, _ ,_ = test(model, val_loader, device)
         val_losses.append(val_loss.item())
     print('Training done!')
-    torch.save(model.state_dict(), 'output/model.pt')
+    torch.save(model.state_dict(), output_path+'model.pt')
 
     # Plot training and validation losses
     plot_losses(train_losses, val_losses, path=dir+'loss.png')
@@ -93,8 +96,30 @@ def main():
 
     """ ---------- Evaluate Model ---------- """
 
-    model.load_state_dict(torch.load('output/model.pt'))
-    val_loss, val_pred, val_true = test(model, val_loader, device)
+    model.load_state_dict(torch.load(output_path+'model.pt'))
+    val_loss, val_pred, val_mol_ids, val_true = test(model, val_loader, device)
+
+    # Compute top1 accuracy
+    pred_top1 = []
+    for id in np.unique(val_mol_ids):
+        mask = id == val_mol_ids
+        idx = np.argpartition(val_pred[mask][:,0], -1)[-1:]
+        if val_true[mask][idx[0]]:
+            pred_top1.append(1)
+        else:
+            pred_top1.append(0)
+    val_acc1 = np.sum(pred_top1)/len(np.unique(val_mol_ids))
+
+    # Compute top2 accuracy
+    pred_top2 = []
+    for id in np.unique(val_mol_ids):
+        mask = id == val_mol_ids
+        idx = np.argpartition(val_pred[mask][:,0], -2)[-2:]
+        if val_true[mask][idx[0]] or val_true[mask][idx[1]]:
+            pred_top2.append(1)
+        else:
+            pred_top2.append(0)
+    val_acc2 = np.sum(pred_top2)/len(np.unique(val_mol_ids))
 
     # Compute and plot precision/recall curve
     PrecisionRecallDisplay.from_predictions(val_true, val_pred)
@@ -104,11 +129,11 @@ def main():
     val_roc_auc = roc_auc_score(val_true, val_pred)
     best_threshold = plot_roc_curve(val_true, val_pred, dir+'roc_curve.png')
 
-    # Compute binary predicted labels from probability predictions with best threshold
+    # Compute binary predictions from probability predictions with best threshold
     val_pred = ((val_pred > best_threshold)[:,0])
 
+    # Compute metrics tht require binry predictions
     val_mcc = matthews_corrcoef(val_true, val_pred)
-    val_acc = accuracy_score(val_true, val_pred)
     val_jacc = jaccard_score(val_true, val_pred)
     val_prec = precision_score(val_true, val_pred, zero_division=0)
     val_rec = recall_score(val_true, val_pred)
@@ -120,7 +145,8 @@ def main():
                 f'Weight decay: {weight_decay}\n'
                 f'Best threshold: {best_threshold}\n'
                 f'Validation MCC: {val_mcc:.2f}\n'
-                f'Validation Accuracy: {val_acc:.2f}\n'
+                f'Validation Top1-Accuracy: {val_acc1:.2f}\n'
+                f'Validation Top2-Accuracy: {val_acc2:.2f}\n'
                 f'Validation Jaccard Score: {val_jacc:.2f}\n'
                 f'Validation Precision: {val_prec:.2f}\n'
                 f'Validation Recall: {val_rec:.2f}\n'
