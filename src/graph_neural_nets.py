4652a0a8457388f6d9088e659b8f498be779ad90
Roxane <a12030167@unet.univie.ac.at>
2022-12-01 16:58:28 +0100

    added non-categorical features

--- src/graph_neural_nets.py
+++ src/graph_neural_nets.py
@@ -1,19 +1,19 @@
 import torch
 import torch.nn.functional as F
-from torch.nn import Sequential, Linear, BatchNorm1d, Dropout,LeakyReLU, AvgPool1d
+from torch.nn import Sequential, Linear, BatchNorm1d, Dropout,LeakyReLU
 from torch_geometric.nn import GINEConv, GATConv, global_add_pool
 
-# class DiceLoss(torch.nn.Module):
-#     def __init__(self):
-#         super(DiceLoss, self).__init__()
+class DiceLoss(torch.nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
 
-#     def forward(self, inputs, targets):
-#         inputs = torch.sigmoid(inputs)
-#         TP = torch.sum(inputs * targets)
-#         FP = torch.sum((1-targets) * inputs)
-#         FN = torch.sum(targets * (1-inputs))
-#         dice_loss = 1 - 2*TP/(2*TP + FP + FN)
-#         return dice_loss
+    def forward(self, inputs, targets):
+        inputs = torch.sigmoid(inputs)
+        TP = torch.sum(inputs * targets)
+        FP = torch.sum((1-targets) * inputs)
+        FN = torch.sum(targets * (1-inputs))
+        dice_loss = 1 - 2*TP/(2*TP + FP + FN)
+        return dice_loss
 
 
 class DiceBCELoss(torch.nn.Module):
@@ -31,79 +31,79 @@ class DiceBCELoss(torch.nn.Module):
         return diceBCE
 
 
-# class JaccardLoss(torch.nn.Module):
-#     def __init__(self):
-#         super(JaccardLoss, self).__init__()
+class JaccardLoss(torch.nn.Module):
+    def __init__(self):
+        super(JaccardLoss, self).__init__()
 
-#     def forward(self, inputs, targets):
-#         inputs = torch.sigmoid(inputs)
-#         TP = torch.sum(inputs * targets)  
-#         FP = torch.sum((1-targets) * inputs)
-#         FN = torch.sum(targets * (1-inputs))
-#         jaccard_loss = 1 - TP/(TP + FP + FN)    
-#         return jaccard_loss
-
-
-# class MCCLoss(torch.nn.Module):
-#     def __init__(self):
-#         super(MCCLoss, self).__init__()
-
-#     def forward(self, inputs, targets):
-#         inputs = torch.sigmoid(inputs)
-#         TP = torch.sum(inputs * targets)
-#         TN = torch.sum((1-inputs)*(1-targets))
-#         FP = torch.sum((1-targets) * inputs)
-#         FN = torch.sum(targets * (1-inputs))
-#         mcc_loss = 1 - (TP*TN - FP*FN)/(torch.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)))  
-#         return mcc_loss
+    def forward(self, inputs, targets):
+        inputs = torch.sigmoid(inputs)
+        TP = torch.sum(inputs * targets)  
+        FP = torch.sum((1-targets) * inputs)
+        FN = torch.sum(targets * (1-inputs))
+        jaccard_loss = 1 - TP/(TP + FP + FN)    
+        return jaccard_loss
 
 
-# class MSELoss(torch.nn.Module):
-#     def __init__(self):
-#         super(MSELoss, self).__init__()
+class MCCLoss(torch.nn.Module):
+    def __init__(self):
+        super(MCCLoss, self).__init__()
 
-#     def forward(self, output, target):
-#         loss = torch.mean((output-target)**2)
-#         return loss
+    def forward(self, inputs, targets):
+        inputs = torch.sigmoid(inputs)
+        TP = torch.sum(inputs * targets)
+        TN = torch.sum((1-inputs)*(1-targets))
+        FP = torch.sum((1-targets) * inputs)
+        FN = torch.sum(targets * (1-inputs))
+        mcc_loss = 1 - (TP*TN - FP*FN)/(torch.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)))  
+        return mcc_loss
 
 
-# class TverskyLoss(torch.nn.Module):
-#     """
-#     For alpha = beta = 0.5, the Tversky index is equivalent to the Dice coefficient, a.k.a. F1 score.
-#     For alpha = beta = 1, the Tversky index is equal to the Tanimoto coefficient.
-#     For alpha + beta = 1, the Tversky index produces the F-beta score. Larger beta values weigh recall higher than precision.
-#     """
-#     def __init__(self, alpha, beta):
-#         super().__init__()
-#         self.alpha = alpha
-#         self.beta = beta
+class MSELoss(torch.nn.Module):
+    def __init__(self):
+        super(MSELoss, self).__init__()
 
-#     def forward(self, inputs, targets):
-#         inputs = torch.sigmoid(inputs)
-#         TP = torch.sum(inputs * targets)
-#         FP = torch.sum((1-targets) * inputs)
-#         FN = torch.sum(targets * (1-inputs))
-#         Tversky = TP / (TP + self.alpha*FP + self.beta*FN)  
-#         return 1 - Tversky
+    def forward(self, output, target):
+        loss = torch.mean((output-target)**2)
+        return loss
+
+
+class TverskyLoss(torch.nn.Module):
+    """
+    For alpha = beta = 0.5, the Tversky index is equivalent to the Dice coefficient, a.k.a. F1 score.
+    For alpha = beta = 1, the Tversky index is equal to the Tanimoto coefficient.
+    For alpha + beta = 1, the Tversky index produces the F-beta score. Larger beta values weigh recall higher than precision.
+    """
+    def __init__(self, alpha, beta):
+        super().__init__()
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, inputs, targets):
+        inputs = torch.sigmoid(inputs)
+        TP = torch.sum(inputs * targets)
+        FP = torch.sum((1-targets) * inputs)
+        FN = torch.sum(targets * (1-inputs))
+        Tversky = TP / (TP + self.alpha*FP + self.beta*FN)  
+        return 1 - Tversky
 
 
 class GIN(torch.nn.Module):
-    def __init__(self, in_dim, h_dim, edge_dim):
+    def __init__(self, in_dim, h_dim, edge_dim, dropout):
         super().__init__()
         self.conv1 = GINEConv(Sequential(Linear(in_dim, h_dim),
                                         BatchNorm1d(h_dim, h_dim),
                                         LeakyReLU(),
-                                        Dropout(p=0.2)
+                                        Dropout(p=dropout)
                                         ), edge_dim=edge_dim)
         self.conv2 = GINEConv(Sequential(Linear(h_dim, h_dim),
                                         BatchNorm1d(h_dim, h_dim),
                                         LeakyReLU(),
-                                        Dropout(p=0.2)
+                                        Dropout(p=dropout)
                                         ), edge_dim=edge_dim)
         self.conv3 = GINEConv(Sequential(Linear(h_dim, h_dim),
                                         BatchNorm1d(h_dim, h_dim),
                                         LeakyReLU(),
-                                        Dropout(p=0.2)
+                                        Dropout(p=dropout)
                                         ), edge_dim=edge_dim)
         self.lin = Sequential(Linear(h_dim*4, h_dim*4),
                               LeakyReLU(),
@@ -131,30 +131,30 @@ class GIN(torch.nn.Module):
 
 
 class GAT(torch.nn.Module):
-    def __init__(self, in_dim, h_dim, num_heads, edge_dim):
+    def __init__(self, in_dim, h_dim, edge_dim, num_heads, neg_slope, dropout):
         super().__init__()
         self.conv1 = GATConv(in_channels=in_dim, 
                             out_channels=h_dim, 
                             heads=num_heads, 
                             concat=True, 
-                            negative_slope=0.2, 
-                            dropout=0.2, 
+                            negative_slope=neg_slope, 
+                            dropout=dropout, 
                             add_self_loops=False, 
                             edge_dim=edge_dim, bias=True)
         self.conv2 = GATConv(in_channels=h_dim * num_heads, 
                             out_channels=h_dim, 
                             heads=num_heads, 
                             concat=True, 
-                            negative_slope=0.2, 
-                            dropout=0.2, 
+                            negative_slope=neg_slope, 
+                            dropout=dropout, 
                             add_self_loops=False, 
                             edge_dim=edge_dim, bias=True)
         self.conv3 = GATConv(in_channels=h_dim * num_heads, 
                             out_channels=h_dim, 
                             heads=num_heads, 
                             concat=True, 
-                            negative_slope=0.2, 
-                            dropout=0.2, 
+                            negative_slope=neg_slope, 
+                            dropout=dropout, 
                             add_self_loops=False, 
                             edge_dim=edge_dim, bias=True)
         self.lin = Sequential(Linear(h_dim*num_heads*4, h_dim*num_heads*4),
@@ -182,27 +182,27 @@ class GAT(torch.nn.Module):
         return h
 
 
-# def train_oversampling(model, loader, lr, wd, device):
-#     model.train()
-#     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
-#     loss_function = torch.nn.BCEWithLogitsLoss()
-#     loss = 0
-#     total_num_instances = 0
-#     for data in loader:
-#         data = data.to(device)
-#         out = model(data.x, data.edge_index, data.edge_attr, data.batch)  # forward pass
-#         num_subsamplings = data.sampling_mask.shape[1]
-#         subsampling_losses = torch.zeros(num_subsamplings)
-#         for i in range(num_subsamplings):
-#             subsampling_losses[i] = loss_function(out[data.sampling_mask[:,i] == 1][:,0].to(float), data.y[data.sampling_mask[:,i] == 1].to(float))
-#         batch_loss = torch.sum(subsampling_losses) / subsampling_losses.size(dim=0)
-#         loss += batch_loss * len(data.batch)
-#         total_num_instances += len(data.batch)
-#         optimizer.zero_grad()  # clear gradients
-#         batch_loss.backward()  # derive gradients
-#         optimizer.step()  # update parameters based on gradients
-#     loss /= total_num_instances
-#     return loss
+def train_oversampling(model, loader, lr, wd, device):
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+    loss_function = torch.nn.BCEWithLogitsLoss()
+    loss = 0
+    total_num_instances = 0
+    for data in loader:
+        data = data.to(device)
+        out = model(data.x, data.edge_index, data.edge_attr, data.batch)
+        num_subsamplings = data.sampling_mask.shape[1]
+        subsampling_losses = torch.zeros(num_subsamplings)
+        for i in range(num_subsamplings):
+            subsampling_losses[i] = loss_function(out[data.sampling_mask[:,i] == 1][:,0].to(float), data.y[data.sampling_mask[:,i] == 1].to(float))
+        batch_loss = torch.sum(subsampling_losses) / subsampling_losses.size(dim=0)
+        loss += batch_loss * len(data.batch)
+        total_num_instances += len(data.batch)
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+    loss /= total_num_instances
+    return loss
 
 def train(model, loader, lr, wd, device):
     model.train()
@@ -212,13 +212,13 @@ def train(model, loader, lr, wd, device):
     total_num_instances = 0
     for data in loader:
         data = data.to(device)
-        out = model(data.x, data.edge_index, data.edge_attr, data.batch)  # forward pass
+        out = model(data.x, data.edge_index, data.edge_attr, data.batch)
         batch_loss = loss_function(out[:,0].to(float), data.y.to(float))
         loss += batch_loss * len(data.batch)
         total_num_instances += len(data.batch)
-        optimizer.zero_grad()  # clear gradients
-        batch_loss.backward()  # derive gradients
-        optimizer.step()  # update parameters based on gradients
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
     loss /= total_num_instances
     return loss
 
--- src/process_input_data.py
+++ src/process_input_data.py
@@ -8,7 +8,8 @@ import os
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Crippen, Descriptors, Lipinski, PandasTools, rdMolDescriptors
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from tqdm import tqdm
 
 def mol_to_nx(mol_id, mol, soms):
@@ -31,7 +32,7 @@ def mol_to_nx(mol_id, mol, soms):
                     degree = atom.GetTotalDegree(),
                     valence = atom.GetTotalValence(),
                     formal_charge = atom.GetFormalCharge(),
-                    #hybridization = atom.GetHybridization(),
+                    hybridization = int(atom.GetHybridization()),
                     num_hs = atom.GetTotalNumHs(),
                     is_in_ring_3 = atom.IsInRingSize(3),
                     is_in_ring_4 = atom.IsInRingSize(4),
@@ -44,10 +45,10 @@ def mol_to_nx(mol_id, mol, soms):
                     covalent_radius = Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()),
                     num_h_acceptors = Lipinski.NumHAcceptors(mol),
                     num_h_donors = Lipinski.NumHDonors(mol),
-                    #molwt = Descriptors.MolWt(mol),
-                    #logp = Crippen.MolLogP(mol),
-                    #tpsa = rdMolDescriptors.CalcTPSA(mol),
-                    #labute_asa = rdMolDescriptors.CalcLabuteASA(mol),
+                    molwt = Descriptors.MolWt(mol),
+                    logp = Crippen.MolLogP(mol),
+                    tpsa = rdMolDescriptors.CalcTPSA(mol),
+                    labute_asa = rdMolDescriptors.CalcLabuteASA(mol),
                     # the next two elements are later used to compute the labels but will of course
                     # not be used as features!
                     mol_id = int(mol_id),
@@ -77,12 +78,12 @@ def compute_node_features_matrix(G):
 
     for i in tqdm(range(num_nodes)):
         current_node = G.nodes[i]
-        atomic_num = [current_node['atomic_num']]
-        degree = [current_node['degree']]
-        valence = [current_node['valence']]
-        formal_charge = [current_node['formal_charge']]
-        #hybridization = [str(current_node['hybridization'])]
-        num_hs = [current_node['num_hs']]
+        atomic_num = [int(current_node['atomic_num'])]
+        degree = [int(current_node['degree'])]
+        valence = [int(current_node['valence'])]
+        formal_charge = [int(current_node['formal_charge'])]
+        hybridization = [int(current_node['hybridization'])]
+        num_hs = [int(current_node['num_hs'])]
         is_in_ring_3 = [int(current_node['is_in_ring_3'])]
         is_in_ring_4 = [int(current_node['is_in_ring_4'])]
         is_in_ring_5 = [int(current_node['is_in_ring_5'])]
@@ -90,29 +91,76 @@ def compute_node_features_matrix(G):
         is_in_ring_7 = [int(current_node['is_in_ring_7'])]
         is_in_ring_8 = [int(current_node['is_in_ring_8'])]
         is_aromatic = [int(current_node['is_aromatic'])]
-        vdw_radius = [current_node['vdw_radius']]
-        covalent_radius = [current_node['covalent_radius']]
-        num_h_acceptors = [current_node['num_h_acceptors']]
-        num_h_donors = [current_node['num_h_donors']]
-        #molwt = [current_node['molwt']]
-        #logp = [current_node['logp']]
-        #tpsa = [current_node['tpsa']]
-        #labute_asa = [current_node['labute_asa']]
+        vdw_radius = [float(current_node['vdw_radius'])]
+        covalent_radius = [float(current_node['covalent_radius'])]
+        num_h_acceptors = [float(current_node['num_h_acceptors'])]
+        num_h_donors = [float(current_node['num_h_donors'])]
+        molwt = [float(current_node['molwt'])]
+        logp = [float(current_node['logp'])]
+        tpsa = [float(current_node['tpsa'])]
+        labute_asa = [float(current_node['labute_asa'])]
 
-        features_vector = atomic_num + degree + valence + formal_charge + \
+        features_vector = atomic_num + degree + valence + formal_charge + hybridization + \
             num_hs + is_in_ring_3 + is_in_ring_4 + is_in_ring_5 + is_in_ring_6 + \
                 is_in_ring_7 + is_in_ring_8 + is_aromatic + vdw_radius + covalent_radius + \
-                    num_h_acceptors + num_h_donors #+ molwt  + logp + tpsa + labute_asa
+                    num_h_acceptors + num_h_donors + molwt + logp + tpsa + labute_asa
 
         if i == 0:
-            features = np.zeros((num_nodes, len(features_vector)), dtype=float)
-        features[i,:] = np.array(features_vector)
+            features = pd.DataFrame({"atomic_num": pd.Series(dtype='int'), 
+                                    "degree": pd.Series(dtype='int'), 
+                                    "valence": pd.Series(dtype='int'), 
+                                    "formal_charge": pd.Series(dtype='int'), 
+                                    "hybridization": pd.Series(dtype='int'), 
+                                    "num_hs": pd.Series(dtype='int'), 
+                                    "is_in_ring_3": pd.Series(dtype='int'), 
+                                    "is_in_ring_4": pd.Series(dtype='int'), 
+                                    "is_in_ring_5": pd.Series(dtype='int'), 
+                                    "is_in_ring_6": pd.Series(dtype='int'), 
+                                    "is_in_ring_7": pd.Series(dtype='int'), 
+                                    "is_in_ring_8": pd.Series(dtype='int'), 
+                                    "is_aromatic": pd.Series(dtype='int'), 
+                                    "vdw_radius": pd.Series(dtype='float'), 
+                                    "covalent_radius": pd.Series(dtype='float'), 
+                                    "num_h_acceptors": pd.Series(dtype='float'), 
+                                    "num_h_donors": pd.Series(dtype='float'), 
+                                    "molwt": pd.Series(dtype='float'), 
+                                    "logp": pd.Series(dtype='float'), 
+                                    "tpsa": pd.Series(dtype='float'), 
+                                    "labute_asa": pd.Series(dtype='float')})
 
-    # One hot encode features
-    ohe = OneHotEncoder(sparse=False, dtype=float, handle_unknown='infrequent_if_exist')
-    features = ohe.fit_transform(features)
+        features.loc[len(features)] = features_vector
 
-    return features
+    features = features.astype({"atomic_num":int, 
+                                "degree":int, 
+                                "valence":int, 
+                                "formal_charge":int, 
+                                "hybridization":int, 
+                                "num_hs":int, 
+                                "is_in_ring_3":int, 
+                                "is_in_ring_4":int, 
+                                "is_in_ring_5":int, 
+                                "is_in_ring_6":int, 
+                                "is_in_ring_7":int, 
+                                "is_in_ring_8":int,
+                                "is_aromatic":int})
+
+    categorical_columns_selector = make_column_selector(dtype_include=int)
+    numerical_columns_selector = make_column_selector(dtype_include=float)
+
+    numerical_columns = numerical_columns_selector(features)
+    categorical_columns = categorical_columns_selector(features)
+
+    categorical_preprocessor = OneHotEncoder(sparse=False, dtype=float, handle_unknown='infrequent_if_exist')
+    numerical_preprocessor = StandardScaler()
+
+    preprocessor = ColumnTransformer([
+        ('one-hot-encoder', categorical_preprocessor, categorical_columns),
+        ('standard_scaler', numerical_preprocessor, numerical_columns)])
+
+    features = preprocessor.fit_transform(features)
+
+    return np.array(features)
+
 
 def process_data(data_directory, data_name):
     """Computes and saves the necessary data (graph, features, labels, graph_ids)
