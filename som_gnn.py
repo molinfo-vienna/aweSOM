232aaa461d2bb265e669a10f35201f1ed3594ec0
Roxane <a12030167@unet.univie.ac.at>
2023-02-07 14:27:09 +0100

    copied branch from personal git repository

--- som_gnn.py
+++ som_gnn.py
@@ -1,17 +1,10 @@
 import argparse
-import ast
-import numpy as np
-import os
 import torch
-from distutils.util import strtobool
-from ipywidgets import widgets
-from rdkit.Chem import Draw, PandasTools
 from sklearn.model_selection import train_test_split
 from torch_geometric import seed_everything
 from torch_geometric.loader import DataLoader
 from torch_geometric.utils import homophily
 
-from src.graph_neural_nets import GIN, GAT, test
 from src.process_input_data import process_data
 from src.pyg_dataset_creator import SOM
 from src.runner import hp_opt, testing
@@ -23,67 +16,134 @@ def main():
     seed_everything(42)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("procedure", nargs="?", default="hp_opt", help="the type of procedure to run: choose between \"process_data\", \"hp_opt\", \"test\"")
-    parser.add_argument("data_directory", nargs="?", default="data/xenosite", help="the folder where the data is stored", type=str)
-    parser.add_argument("data", nargs="?", default="xenosite.sdf", help="the name of the data file (must be a .sdf file)", type=str)
-    parser.add_argument("output_directory", nargs="?", default="output/xenosite", help="the folder where the results will be stored", type=str)
-    parser.add_argument("results_file_name", nargs="?", default="results.csv", help="the name of the csv file to store the summarized results", type=str)
-    parser.add_argument("model_name", nargs="?", default= "GIN", help="the neural network that will be used: either \"GIN\" (Graph Isomorphism Network) or \"GAT\" (Graph Attention Network", type=str)
-    parser.add_argument("h_dim", nargs="?", default=32, help="the size of the hidden layers", type=int)
-    parser.add_argument("dropout", nargs="?", default=0.2, help="dropout probability", type=float)
-    parser.add_argument("num_heads", nargs="?", default=0, help="the number of heads for the GAT model (ignore when using GIN)", type=int)
-    parser.add_argument("neg_slope", nargs="?", default=0, help="steepness of the negative slope for the GAT model (ignore when using GIN)", type=float)
-    parser.add_argument("epochs", nargs="?", default=1000, help="the number of training epochs", type=int)
+    parser.add_argument(
+        "procedure",
+        nargs="?",
+        default="testing",
+        help='the type of procedure to run: choose between "process_data", "hp_opt", "testing"',
+    )
+    parser.add_argument(
+        "data_directory",
+        nargs="?",
+        default="data_zaretzki",
+        help="the folder where the data is stored",
+        type=str,
+    )
+    parser.add_argument(
+        "data",
+        nargs="?",
+        default="data.sdf",
+        help="the name of the data file (must be .sdf file)",
+        type=str,
+    )
+    parser.add_argument(
+        "output_directory",
+        nargs="?",
+        default="../zaretzki_output_test/S1",
+        help="the folder where the results will be stored",
+        type=str,
+    )
+    parser.add_argument(
+        "saved_models_summary_file",
+        nargs="?",
+        default="example.csv",
+        help="the csv file holding metadata of the models chosen for the ensemble classifier",
+        type=str,
+    )
+    parser.add_argument(
+        "h_dim", nargs="?", default=64, help="the size of the hidden layers", type=int
+    )
+    parser.add_argument(
+        "dropout", nargs="?", default=0.1, help="dropout probability", type=float
+    )
+    parser.add_argument(
+        "epochs",
+        nargs="?",
+        default=1000,
+        help="the maximum number of training epochs",
+        type=int,
+    )
     parser.add_argument("lr", nargs="?", default=1e-3, help="learning rate", type=float)
-    parser.add_argument("wd", nargs="?", default=1e-3, help="weight decay", type=float)
-    parser.add_argument("batch_size", nargs="?", default=32, help="batch size", type=int)
-    parser.add_argument("oversampling", nargs="?", default=False, help="whether to use oversampling technique or not: [True/False]", type=lambda x: bool(strtobool(x)))
-    parser.add_argument("size_avg_window", nargs="?", default=10, help="early stopping: size of the interval taken into account to compute loss average", type=int)
-    parser.add_argument("patience", nargs="?", default=5, help="early stopping: number of early stoppping evaluation phases with no improvement after which training will be stopped", type=int)
-    parser.add_argument("delta", nargs="?", default=0., help="early stopping: minimal ratio to qualify as improvement", type=float)
+    parser.add_argument("wd", nargs="?", default=0, help="weight decay", type=float)
+    parser.add_argument("batch_size", nargs="?", default=8, help="batch size", type=int)
+    parser.add_argument(
+        "patience",
+        nargs="?",
+        default=5,
+        help="early stopping: number of epochs with no improvement of the \
+                        validation or test loss after which training will be stopped",
+        type=int,
+    )
+    parser.add_argument(
+        "delta",
+        nargs="?",
+        default=5,
+        help="early stopping: minimum change in the monitored quantity to qualify as an improvement, \
+                        i.e. an absolute change of less than delta will count as no improvement",
+        type=float,
+    )
     args = parser.parse_args()
 
     if args.procedure == "process_data":
-        process_data(args.data_directory, args.data)  # process SDF input data to create PyTorch Geometric custom dataset
+        # process SDF input data to create PyTorch Geometric custom dataset
+        process_data(args.data_directory, args.data)
 
     else:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Create/Load Custom PyTorch Geometric Dataset
         dataset = SOM(root=args.data_directory)
 
         # Print dataset info
-        print(f'Number of graphs: {len(dataset)}')
-        print(f'Number of node features: {dataset.num_node_features}')
-        print(f'Number of edge features: {dataset.num_edge_features}')
-        print(f'Number of classes: {dataset.num_classes}')
+        print(f"Number of graphs: {len(dataset)}")
+        print(f"Number of node features: {dataset.num_node_features}")
+        print(f"Number of edge features: {dataset.num_edge_features}")
+        print(f"Number of classes: {dataset.num_classes}")
 
         # Compute homophily
-        loader = DataLoader(dataset, batch_size=len(dataset))
-        for data in loader: print(f'Homophily: {homophily(data.edge_index, data.y):.2f}')
+        # loader = DataLoader(dataset, batch_size=len(dataset))
+        # for data in loader:
+        #     print(f"Homophily: {homophily(data.edge_index, data.y):.2f}")
 
         # Training/Evaluation/Test Split
-        train_val_data, test_data = train_test_split(dataset, test_size=1/10, random_state=42, shuffle=True)
-        train_data, val_data = train_test_split(train_val_data, test_size=1/9, random_state=42, shuffle=True)
-        print(f'Training set: {len(train_data)} molecules.')
-        print(f'Validation set: {len(val_data)} molecules.')
-        print(f'Test set: {len(test_data)} molecules.')
+        train_val_data, test_data = train_test_split(
+            dataset, test_size=1 / 10, random_state=42, shuffle=True
+        )
+
+        # train_val_molids = [item.mol_id[0].item() for item in train_val_data]
+        # with open('data/train_val_molids.txt', 'w') as f:
+        #     for item in train_val_molids:
+        #         f.write("%s\n" % item)
+        # test_molids = [item.mol_id[0].item() for item in test_data]
+        # with open('data/test_molids.txt', 'w') as f:
+        #     for item in test_molids:
+        #         f.write("%s\n" % item)
 
         if args.procedure == "hp_opt":
-            hp_opt(device, dataset, train_data, val_data, \
-                    args.output_directory, args.results_file_name, \
-                        args.data, args.model_name, args.h_dim, args.dropout, \
-                            args.num_heads, args.neg_slope, args.epochs, \
-                                args.lr, args.wd, args.batch_size, args.oversampling, \
-                                    args.size_avg_window, args.patience, args.delta)
+            hp_opt(
+                device,
+                dataset,
+                train_val_data,
+                args.output_directory,
+                args.h_dim,
+                args.dropout,
+                args.epochs,
+                args.lr,
+                args.wd,
+                args.batch_size,
+                args.patience,
+                args.delta,
+            )
 
-        if args.procedure == "test":
-            testing(device, dataset, train_data, test_data, \
-                        args.output_directory, \
-                            args.data, args.model_name, args.h_dim, args.dropout, \
-                                args.num_heads, args.neg_slope, args.epochs, \
-                                    args.lr, args.wd, args.batch_size, args.oversampling, \
-                                        args.size_avg_window, args.patience, args.delta)
+        if args.procedure == "testing":
+            testing(
+                device,
+                dataset,
+                test_data,
+                args.output_directory,
+                args.saved_models_summary_file,
+            )
+
 
 if __name__ == "__main__":
     main()
--- src/graph_neural_nets.py
+++ src/graph_neural_nets.py
@@ -1,113 +1,76 @@
 import torch
 import torch.nn.functional as F
-from torch.nn import Sequential, Linear, BatchNorm1d, Dropout,LeakyReLU
-from torch_geometric.nn import GINEConv, GATConv, global_add_pool
+from torch.nn import Sequential, Linear, BatchNorm1d, Dropout, LeakyReLU
+from torch_geometric.nn import GINEConv, global_add_pool
 
-class DiceLoss(torch.nn.Module):
+
+class MCC_Loss(torch.nn.Module):
     def __init__(self):
-        super(DiceLoss, self).__init__()
+        super(MCC_Loss, self).__init__()
 
     def forward(self, inputs, targets):
         inputs = torch.sigmoid(inputs)
         TP = torch.sum(inputs * targets)
-        FP = torch.sum((1-targets) * inputs)
-        FN = torch.sum(targets * (1-inputs))
-        dice_loss = 1 - 2*TP/(2*TP + FP + FN)
-        return dice_loss
+        TN = torch.sum((1 - inputs) * (1 - targets))
+        FP = torch.sum((1 - targets) * inputs)
+        FN = torch.sum(targets * (1 - inputs))
+        MCC_loss = 1 - (TP * TN - FP * FN) / (
+            torch.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+        )
+        return MCC_loss
 
 
-class DiceBCELoss(torch.nn.Module):
+class MCC_BCE_Loss(torch.nn.Module):
     def __init__(self):
-        super(DiceBCELoss, self).__init__()
-
-    def forward(self, inputs, targets):
-        inputs = torch.sigmoid(inputs)       
-        TP = torch.sum(inputs * targets)
-        FP = torch.sum((1-targets) * inputs)
-        FN = torch.sum(targets * (1-inputs))
-        dice_loss = 1 - 2*TP/(2*TP + FP + FN)
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='sum')
-        diceBCE = BCE + dice_loss
-        return diceBCE
-
-
-class JaccardLoss(torch.nn.Module):
-    def __init__(self):
-        super(JaccardLoss, self).__init__()
-
-    def forward(self, inputs, targets):
-        inputs = torch.sigmoid(inputs)
-        TP = torch.sum(inputs * targets)  
-        FP = torch.sum((1-targets) * inputs)
-        FN = torch.sum(targets * (1-inputs))
-        jaccard_loss = 1 - TP/(TP + FP + FN)    
-        return jaccard_loss
-
-
-class MCCLoss(torch.nn.Module):
-    def __init__(self):
-        super(MCCLoss, self).__init__()
+        super(MCC_BCE_Loss, self).__init__()
 
     def forward(self, inputs, targets):
         inputs = torch.sigmoid(inputs)
         TP = torch.sum(inputs * targets)
-        TN = torch.sum((1-inputs)*(1-targets))
-        FP = torch.sum((1-targets) * inputs)
-        FN = torch.sum(targets * (1-inputs))
-        mcc_loss = 1 - (TP*TN - FP*FN)/(torch.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)))  
-        return mcc_loss
-
-
-class MSELoss(torch.nn.Module):
-    def __init__(self):
-        super(MSELoss, self).__init__()
-
-    def forward(self, output, target):
-        loss = torch.mean((output-target)**2)
-        return loss
-
-
-class TverskyLoss(torch.nn.Module):
-    """
-    For alpha = beta = 0.5, the Tversky index is equivalent to the Dice coefficient, a.k.a. F1 score.
-    For alpha = beta = 1, the Tversky index is equal to the Tanimoto coefficient.
-    For alpha + beta = 1, the Tversky index produces the F-beta score. Larger beta values weigh recall higher than precision.
-    """
-    def __init__(self, alpha, beta):
-        super().__init__()
-        self.alpha = alpha
-        self.beta = beta
-
-    def forward(self, inputs, targets):
-        inputs = torch.sigmoid(inputs)
-        TP = torch.sum(inputs * targets)
-        FP = torch.sum((1-targets) * inputs)
-        FN = torch.sum(targets * (1-inputs))
-        Tversky = TP / (TP + self.alpha*FP + self.beta*FN)  
-        return 1 - Tversky
+        TN = torch.sum((1 - inputs) * (1 - targets))
+        FP = torch.sum((1 - targets) * inputs)
+        FN = torch.sum(targets * (1 - inputs))
+        MCC_loss = 1 - (TP * TN - FP * FN) / (
+            torch.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+        )
+        BCE_loss = F.binary_cross_entropy(inputs, targets, reduction="sum")
+        MCC_BCE_loss = MCC_loss + BCE_loss
+        return MCC_BCE_loss
 
 
 class GIN(torch.nn.Module):
     def __init__(self, in_dim, h_dim, edge_dim, dropout):
         super().__init__()
-        self.conv1 = GINEConv(Sequential(Linear(in_dim, h_dim),
-                                        BatchNorm1d(h_dim, h_dim),
-                                        LeakyReLU(),
-                                        Dropout(p=dropout)
-                                        ), edge_dim=edge_dim)
-        self.conv2 = GINEConv(Sequential(Linear(h_dim, h_dim),
-                                        BatchNorm1d(h_dim, h_dim),
-                                        LeakyReLU(),
-                                        Dropout(p=dropout)
-                                        ), edge_dim=edge_dim)
-        self.conv3 = GINEConv(Sequential(Linear(h_dim, h_dim),
-                                        BatchNorm1d(h_dim, h_dim),
-                                        LeakyReLU(),
-                                        Dropout(p=dropout)
-                                        ), edge_dim=edge_dim)
-        self.lin = Sequential(Linear(h_dim*4, h_dim*4),
-                              LeakyReLU(),
-                              Linear(h_dim*4, 1))
+        self.conv1 = GINEConv(
+            Sequential(
+                Linear(in_dim, h_dim),
+                BatchNorm1d(h_dim, h_dim),
+                LeakyReLU(),
+                Dropout(p=dropout),
+            ),
+            edge_dim=edge_dim,
+        )
+        self.conv2 = GINEConv(
+            Sequential(
+                Linear(h_dim, h_dim),
+                BatchNorm1d(h_dim, h_dim),
+                LeakyReLU(),
+                Dropout(p=dropout),
+            ),
+            edge_dim=edge_dim,
+        )
+        self.conv3 = GINEConv(
+            Sequential(
+                Linear(h_dim, h_dim),
+                BatchNorm1d(h_dim, h_dim),
+                LeakyReLU(),
+                Dropout(p=dropout),
+            ),
+            edge_dim=edge_dim,
+        )
+        self.lin = Sequential(
+            Linear(h_dim * 4, h_dim * 4), LeakyReLU(), Linear(h_dim * 4, 1)
+        )
 
     def forward(self, x, edge_index, edge_attr, batch):
 
@@ -122,98 +85,24 @@ class GIN(torch.nn.Module):
         h_pool_ = torch.repeat_interleave(h_pool, num_atoms_per_mol, dim=0)
 
         # Concatenate embeddings
-        h = torch.cat((h1,h2,h3,h_pool_), dim=1)
+        h = torch.cat((h1, h2, h3, h_pool_), dim=1)
 
         # Classify
         h = self.lin(h)
 
         return h
 
-
-class GAT(torch.nn.Module):
-    def __init__(self, in_dim, h_dim, edge_dim, num_heads, neg_slope, dropout):
-        super().__init__()
-        self.conv1 = GATConv(in_channels=in_dim, 
-                            out_channels=h_dim, 
-                            heads=num_heads, 
-                            concat=True, 
-                            negative_slope=neg_slope, 
-                            dropout=dropout, 
-                            add_self_loops=False, 
-                            edge_dim=edge_dim, bias=True)
-        self.conv2 = GATConv(in_channels=h_dim * num_heads, 
-                            out_channels=h_dim, 
-                            heads=num_heads, 
-                            concat=True, 
-                            negative_slope=neg_slope, 
-                            dropout=dropout, 
-                            add_self_loops=False, 
-                            edge_dim=edge_dim, bias=True)
-        self.conv3 = GATConv(in_channels=h_dim * num_heads, 
-                            out_channels=h_dim, 
-                            heads=num_heads, 
-                            concat=True, 
-                            negative_slope=neg_slope, 
-                            dropout=dropout, 
-                            add_self_loops=False, 
-                            edge_dim=edge_dim, bias=True)
-        self.lin = Sequential(Linear(h_dim*num_heads*4, h_dim*num_heads*4),
-                              LeakyReLU(),
-                              Linear(h_dim*num_heads*4, 1))
-
-    def forward(self, x, edge_index, edge_attr, batch):
-
-        # Node embeddings
-        h1 = self.conv1(x, edge_index, edge_attr)
-        h2 = self.conv2(h1, edge_index, edge_attr)
-        h3 = self.conv3(h2, edge_index, edge_attr)
-
-        # Pooling
-        h_pool = global_add_pool(h3, batch)
-        num_atoms_per_mol = torch.unique(batch, sorted=False, return_counts=True)[1]
-        h_pool_ = torch.repeat_interleave(h_pool, num_atoms_per_mol, dim=0)
-
-        # Concatenate embeddings
-        h = torch.cat((h1,h2,h3,h_pool_), dim=1)
-
-        # Classify
-        h = self.lin(h)
-
-        return h
-
-
-def train_oversampling(model, loader, lr, wd, device):
-    model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
-    loss_function = torch.nn.BCEWithLogitsLoss()
-    loss = 0
-    total_num_instances = 0
-    for data in loader:
-        data = data.to(device)
-        out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        num_subsamplings = data.sampling_mask.shape[1]
-        subsampling_losses = torch.zeros(num_subsamplings)
-        for i in range(num_subsamplings):
-            subsampling_losses[i] = loss_function(out[data.sampling_mask[:,i] == 1][:,0].to(float), data.y[data.sampling_mask[:,i] == 1].to(float))
-        batch_loss = torch.sum(subsampling_losses) / subsampling_losses.size(dim=0)
-        loss += batch_loss * len(data.batch)
-        total_num_instances += len(data.batch)
-        optimizer.zero_grad()
-        batch_loss.backward()
-        optimizer.step()
-    loss /= total_num_instances
-    return loss
 
 def train(model, loader, lr, wd, device):
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
-    loss_function = DiceBCELoss()
+    loss_function = MCC_BCE_Loss()
     loss = 0
     total_num_instances = 0
     for data in loader:
         data = data.to(device)
         out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        batch_loss = loss_function(out[:,0].to(float), data.y.to(float))
+        batch_loss = loss_function(out[:, 0].to(float), data.y.to(float))
         loss += batch_loss * len(data.batch)
         total_num_instances += len(data.batch)
         optimizer.zero_grad()
@@ -222,25 +111,32 @@ def train(model, loader, lr, wd, device):
     loss /= total_num_instances
     return loss
 
+
 @torch.no_grad()
 def test(model, loader, device):
     model.eval()
-    loss_function = DiceBCELoss()
+    loss_function = MCC_BCE_Loss()
     loss = 0
     total_num_instances = 0
     y_preds = []
     mol_ids = []
+    atom_ids = []
     y_trues = []
     for data in loader:
         data = data.to(device)
         out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        batch_loss = loss_function(out[:,0].to(float), data.y.to(float))
+        batch_loss = loss_function(out[:, 0].to(float), data.y.to(float))
         loss += batch_loss * len(data.batch)
         total_num_instances += len(data.batch)
         y_preds.append(torch.sigmoid(out))
         mol_ids.append(data.mol_id)
+        atom_ids.append(data.atom_id)
         y_trues.append(data.y)
-    y_pred, mol_id, y_true = torch.cat(y_preds, dim=0).cpu().numpy(), torch.cat(mol_ids, dim=0).cpu().numpy(), torch.cat(y_trues, dim=0).cpu().numpy()
+    y_pred, mol_id, atom_id, y_true = (
+        torch.cat(y_preds, dim=0).cpu().numpy(),
+        torch.cat(mol_ids, dim=0).cpu().numpy(),
+        torch.cat(atom_ids, dim=0).cpu().numpy(),
+        torch.cat(y_trues, dim=0).cpu().numpy(),
+    )
     loss /= total_num_instances
-    return loss, y_pred, mol_id, y_true
-   
+    return loss, y_pred, mol_id, atom_id, y_true
