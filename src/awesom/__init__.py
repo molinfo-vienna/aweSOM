45a02d0919f11673eb14cda43c04b64f3171a712
Leo Gaskin <leo.gaskin@le0.gs>
2026-06-11 17:32:22 +0200

    Export fewer items

--- src/awesom/__init__.py
+++ src/awesom/__init__.py
@@ -1,9 +1,5 @@
 from .dataset import SOMDataset
-from .gpu_utils import get_device, print_device_info
-from .metrics import (
-    MetricsCalculator,
-    ResultsLogger,
-)
+from .metrics import MetricsCalculator, ResultsLogger
 from .model import (
     EnsemblePredictions,
     GINEWithContextPooling,
@@ -15,11 +11,8 @@ __all__ = [
     "SOMDataset",
     "SOMPredictor",
     "GINEWithContextPooling",
-    "get_device",
     "predict_ensemble",
-    "print_device_info",
     "EnsemblePredictions",
-    "log_results",
     "MetricsCalculator",
     "ResultsLogger",
 ]
